ce85097441920fad77270bbca3ee67ee6e36cd03
frederick rosales <frederick.rosales@gmail.com>
2022-04-11 18:20:44 -0400

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/rikriki/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('rikriki')
+            node / gitConfigEmail('frederick.rosales@gmail.com')
         }
     }
     triggers {
